5dee94063ec673b367b52b8d65cbcb3c9f04cf2b
gabriel.vitoria <gabriel.vitoria.ti@gmail.com>
2017-05-30 00:32:44 -0300

    Sessao 4 - aula 25 add bootstrap 6

--- app/app.components.ts
+++ app/app.components.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
     selector:'my-app',
-    template :'<h1>My first Angularjs 2</h1>'
+    template :`
+            <h1>My first Angularjs 2 Muito massa</h1>
+            <button class="btn btn-success"> Que isso lindo de mais</button>
+    `
 })
 export class AppComponent {}
