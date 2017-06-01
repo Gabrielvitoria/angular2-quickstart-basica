a4a5d962d701da7c6fbb35933c2343fd545e2595
gabriel.vitoria <gabriel.vitoria.ti@gmail.com>
2017-05-31 22:25:57 -0300

    Aula 4 -26 - Criando novo módulo de recurso

--- app/app.components.ts
+++ app/app.components.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
     selector:'my-app',
     template :`
-            <h1>My first Angularjs 2 Muito massa</h1>
-            <button class="btn btn-success"> Que isso lindo de mais</button>
+            <h2>Curso Angularjs 2</h2>
+            <contatos-lista>
+            </contatos-lista>
     `
 })
 export class AppComponent {}
--- app/contatos/contatos.module.ts
+++ app/contatos/contatos.module.ts
@@ -1,8 +1,15 @@
 import { NgModule} from '@angular/core';
-
+import { ContatosListaComponet} from './contatos-lista.componet';
 
 /*Decorator de acordo com as necessidades */
 @NgModule({
-    
+    declarations:[
+        ContatosListaComponet
+    ],
+    exports:[
+        ContatosListaComponet
+    ]
 })
+
+
 export class ContatosModule{}
