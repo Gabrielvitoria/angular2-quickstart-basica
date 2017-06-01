3b722e31cb523264431bd8013fcd3c2fdd4b3554
gabriel.vitoria <gabriel.vitoria.ti@gmail.com>
2017-05-31 22:30:41 -0300

    Aula 4 -28 Criando Classe de Modelo Contato

---
+++ app/contatos/contato.model.ts
@@ -0,0 +1,9 @@
+export class Contato{
+/*Dessa forma não precisa realizar o mapeamento de cada prop
+O TS ja faz o processo de bing.  */
+    constructor(
+        public id: number,
+        public nome:string,
+        public telefone:string
+    ){}
+}
