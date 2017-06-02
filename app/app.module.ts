475c6aaf1ba0711998ca5ad55f6fbb9ca394f1a4
gabriel.vitoria <gabriel.vitoria.ti@gmail.com>
2017-06-02 00:06:19 -0300

    Aula 04-32: Criação do Modulo de Rota: Nesse commit o console apresenta um erro. Pois na proxima aula que será criado a rota que está sendo acessada.

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,10 +1,14 @@
+// Seguindo o Style guie do Angula2 Modulos para adicionar rotas na aplicação
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.components';
+import { AppRoutingModule } from './app-routing.module';
 import { ContatosModule } from './contatos/contatos.module';
+
 
 @NgModule({
     imports: [
+        AppRoutingModule,
         BrowserModule,
         ContatosModule
     ],
