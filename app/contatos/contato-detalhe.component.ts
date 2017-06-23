73378c467d5033f98106e4ac85956ee3c446a7ae
gabriel.vitoria <gabriel.vitoria.ti@gmail.com>
2017-06-22 22:21:19 -0300

    captura o parametro da rota.

--- app/contatos/contato-detalhe.component.ts
+++ app/contatos/contato-detalhe.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+import { ContatoService } from './contato.service';
+
+
 
 import { Classificacao } from '../classificacao/classificacao.Model';
 import { ClassificacaoService } from '../classificacao/classificacao.service';
@@ -10,13 +15,28 @@ import { ClassificacaoService } from '../classificacao/classificacao.service';
 })
 
 export class ContatoDetalheComponent {
-    classificacoes : Classificacao[];
+    //classificacoes : Classificacao[];
+    //constructor(private ClassificacaoService: ClassificacaoService) { }
 
-    constructor(private ClassificacaoService: ClassificacaoService) { }
-    
+    constructor(
+        private contatoService: ContatoService,
+        private route: ActivatedRoute,
+        private location: Location
+    ) { }
+
+
     ngOnInit(): void {
         //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
         //Add 'implements OnInit' to the class.
-        this.classificacoes = this.ClassificacaoService.listar();
+        // this.classificacoes = this.ClassificacaoService.listar();
+
+        /* route.params.forEach: é um observable e o + params converte pararetornar um interiro */
+
+        this.route.params.forEach((params: Params)=>{
+            let id: number = +params["id"]
+            console.log(id);
+
+        });
+
     }
 }
