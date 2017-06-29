ddd7b3a2cb8ca8d87c4807695991aea488609a96
gabriel.vitoria <gabriel.vitoria.ti@gmail.com>
2017-06-28 23:42:38 -0300

    RXJS para converver observable em promisse no serviço de gerContatos

--- app/contatos/contato.service.ts
+++ app/contatos/contato.service.ts
@@ -8,10 +8,27 @@ import { Injectable } from '@angular/core';
 import { CONTATOS } from './contatos-mock';
 import { Contato } from './contato.model';
 
+import "rxjs/add/operator/toPromise";
+
+import {Http} from '@angular/http';
+
 @Injectable()
 export class ContatoService {
+
+    private contatosUrl:string = 'app/contatos';
+
+    constructor(
+        private http:Http
+        
+    ){}
+
+
     getContatos():Promise<Contato[]> {
-        return Promise.resolve(CONTATOS);
+        return this.http.get(this.contatosUrl)
+            .toPromise()
+            .then(response => response.json().data as Contato[]);
+
+        //return Promise.resolve(CONTATOS);
     }
 
 /*Como exemplo:  Na real utilziaria uma api que retorna por id app/conato/id */
